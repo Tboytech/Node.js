18c3c620ef33ed9dd57618a348daaab77433f3a1
olayinka babaunde <babatundeolayinka28@gmail.com>
2024-06-27 12:05:15 +0100

    node

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require("http")
+
+http
+.createServer((req, res)=>{
+    // send the HTTP header
+    // HTTP Status: 200 : OK 
+    // Content Type: text/plain
+res.writeHead(200, {"content-type": "text/html"})
+// send the response body as "Hello world"
+res.write(`<h1>Hello world!</h1>`)
+res.end()
+}).listen(3000)
+
+console.log("Server is running on port http://localhost:3000/");
