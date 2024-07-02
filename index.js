bc8b1ca534f062d61430910f5e959d6cc66efbd8
olayinka babaunde <babatundeolayinka28@gmail.com>
2024-07-02 10:05:47 +0100

    node

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const http = require("http")
+//
+// fs.unlink(`input.txt`, function(err){
+//     if (err) {
+//         return console.log(object);
+//     }
+//     console.log("Directory created successfully");
+// })
 
-http
-.createServer((req, res)=>{
-    // send the HTTP header
-    // HTTP Status: 200 : OK 
-    // Content Type: text/plain
-res.writeHead(200, {"content-type": "text/html"})
-// send the response body as "Hello world"
-res.write(`<h1>Hello TBOY!</h1>`)
-res.end()
-}).listen(3000)
 
-console.log("Server is running on port http://localhost:3000/");
+
+
+const fs = require("fs");
+
+const data = fs.readFileSync("./text/data.txt");
+fs.writeFile("./text/data.txt", 'just wrote this', (err, data)=>{
+    if (err) {
+        console.log(err);
+    }
+    console.log("file written successfully");
+})
+
+fs.readFile("./text/data.txt", (error, data) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(data.toString(), "async");
+});
+
+// console.log(data.toString());
+
+
+console.log("Program as ended");
+
+fs.mkdir(`./test`, function(err){
+    if (err) {
+        return console.log(err);
+    }
+    console.log("file created successfully");
+})
